a45af644632ba36af81bc86d6b4b8fd4a9bd4e24
Thay Mendes <thay.gm@live.com>
2024-04-24 14:35:32 -0300

    WIP

--- src/Container/ListaTarefas/index.tsx
+++ src/Container/ListaTarefas/index.tsx
@@ -1,25 +1,42 @@
 import Tarefa from '../../components/Tarefa'
+import { Titulo } from '../../components/Tarefa/styles'
 import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'Estudar Vuejs',
+    descricao: 'Verificar as novas atualizações',
+    prioridade: 'Importante',
+    status: 'Pendente'
+  },
+  {
+    titulo: 'Fazer Imposto de Renda',
+    descricao: 'dar baixa no imposto',
+    prioridade: 'Urgente',
+    status: 'Concluída'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'LEGDAY',
+    prioridade: 'Importante',
+    status: 'Pendente'
+  }
+]
 
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome da Tarefa</S.Titulo>
-    <S.Tag>Importante</S.Tag>
-    <S.Tag>Pendente</S.Tag>
-    <S.Descricao />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+type TarefaProps = {
+  titulo: string
+  prioridade: string
+  status: string
+  descricao: string
+}
 
+const Tarefa = ({ descricao, prioridade, status, titulo }: TarefaProps) => {
+  const [estaEditado, setEstaEditado] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag>{prioridade}</S.Tag>
+      <S.Tag>{status}</S.Tag>
+      <S.Descricao value={descricao} />
+      <S.BarraAcoes>
+        {estaEditado ? (
+          <>
+            <S.Botao>Salvar</S.Botao>
+            <S.Botao onClick={() => setEstaEditado(false)}>Cancelar</S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditado(true)}>Editar</S.Botao>
+            <S.Botao>Remover</S.Botao>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 export default Tarefa
