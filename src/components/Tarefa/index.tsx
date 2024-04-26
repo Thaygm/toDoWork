0279478587a42fd07c9ab25ba5fd5410d3f8c5b6
Thay Mendes <thay.gm@live.com>
2024-04-26 08:36:48 -0300

    edit tag

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import * as S from './styles'
-import { remover, editar } from '../../store/reducers/tarefas'
+import { remover, editar, alteraStatus } from '../../store/reducers/tarefas'
 import { useDispatch } from 'react-redux'
 import TarefaClass from '../../models/tarefas'
 import { BotaoSalvar } from '../../styles'
+import * as enums from '../../utils/enums/Tarefa'
 
 type TarefaProps = TarefaClass
 
@@ -29,9 +30,29 @@ const Tarefa = ({
     setDescricao(descricaoOriginal)
   }
 
+  function alteraStatusTarefa(evento: ChangeEvent<HTMLInputElement>) {
+    dispatch(
+      alteraStatus({
+        id,
+        finalizado: evento.target.checked
+      })
+    )
+  }
+
   return (
     <S.Card>
-      <S.Titulo>{titulo}</S.Titulo>
+      <label htmlFor={titulo}>
+        <input
+          type="checkbox"
+          id={titulo}
+          checked={status === enums.Status.CONCLUIDA}
+          onChange={alteraStatusTarefa}
+        />
+        <S.Titulo>
+          {estaEditando && <em>Editando: </em>}
+          {titulo}
+        </S.Titulo>
+      </label>
       <S.Tag parametro="prioridade" prioridade={prioridade}>
         {prioridade}
       </S.Tag>
